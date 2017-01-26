3da4c11f416b5ce8726986dbc01d5eb776e85cdf
Dragutin Cvetkovic <drazha@maildrome.com>
2017-01-26 14:30:37 +0000

    Modified

--- Witty_ESP-12F_DHT11_n_Light_Sensor_to_ThingSpeak/Witty_ESP-12F_DHT11_n_Light_Sensor_to_ThingSpeak.ino
+++ Witty_ESP-12F_DHT11_n_Light_Sensor_to_ThingSpeak/Witty_ESP-12F_DHT11_n_Light_Sensor_to_ThingSpeak.ino
@@ -7,23 +7,25 @@
  *   Version 1.0  25/01/2016  Version 1.0    Dragutin Cvetkovic
  */
 
-#include <DHT.h>
-#include <ESP8266WiFi.h>
 
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#define DHTTYPE DHT11
+#define DHTPIN  14
 #define LDR     A0
 #define BUTTON  4
 #define RED     15
 #define BLUE    12
 #define GREEN   13
-#define DHTPIN  14    // Make sure that the PIN used for DHT input is not already used by the Witty Board 
 
-#define DHTTYPE DHT11 // Change to proper DHT type here
 
 // replace with your channel’s thingspeak API key and your SSID and password
 String apiKey = "YUNNBF1UYS1B5IGV";
 const char* ssid = "IoTtest";
 const char* password = "dr4gut1n";
 const char* server = "api.thingspeak.com";
+
+WiFiClient client;
 
 // Initialize DHT sensor 
 // NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
@@ -35,13 +37,14 @@ const char* server = "api.thingspeak.com";
 // This is for the ESP8266 processor on ESP-01 
 DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
 
-WiFiClient client;
+float h, t, ldr, humidity, temp; // Values read from the sensor
 
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
 
 void setup(void) {
-{
+
     Serial.begin(115200);
-    delay(10);
     dht.begin();
 
     WiFi.begin(ssid, password);
@@ -62,31 +65,31 @@ void setup(void) {
     Serial.println("WiFi connected");
 
     pinMode(LDR, INPUT);
-    pinMode(BLINK, OUTPUT);
+    pinMode(GREEN, OUTPUT);
 }
 
 void loop(void) {
 
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    if (isnan(h) || isnan(t))
-    {
-        Serial.println("Failed to read from DHT sensor!");
-        //return;
-    }
+//    h = dht.readHumidity();
+//    t = dht.readTemperature();
+//    if (isnan(h) || isnan(t))
+//    {
+//        Serial.println("Failed to read from DHT sensor!");
+//        //return;
+//    }
+//
+//    ldr = analogRead(LDR);
 
-    float ldr = analogRead(LDR);
 
-
-
+    getSensors();           // read sensors
 
 
     if (client.connect(server,80)) {
         String postStr = apiKey;
         postStr +="&field1=";
-        postStr += String(t);
+        postStr += String(temp);
         postStr +="&field2=";
-        postStr += String(h);
+        postStr += String(humidity);
         postStr += "&field3=";
         postStr += String(ldr);
         postStr += "\r\n\r\n";
@@ -103,24 +106,60 @@ void loop(void) {
         client.print(postStr);
 
         Serial.print("Temperature: ");
-        Serial.println(t);
+        Serial.println(temp);
         Serial.print("Humidity: ");
-        Serial.println(h);
+        Serial.println(humidity);
         Serial.print("LDR: ");
         Serial.println(ldr);
         Serial.println("Sending data to Thingspeak");
     }
     client.stop();
 
-    Serial.println("Waiting 20 secs");
+//    Serial.println("Waiting 20 secs");
 // thingspeak needs at least a 15 sec delay between updates
 // 20 seconds to be safe
 //    delay(20000);
 
-    digitalWrite(BLINK, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(19000);              // wait for a second
-    digitalWrite(BLINK, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(GREEN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(GREEN, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);              // wait for a second
 
     
+}
+
+void getSensors() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    Serial.println("Reading humidity");
+    humidity = dht.readHumidity();          // Read humidity (percent)
+
+    Serial.println("Reading temperature");
+    //temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
+    temp = dht.readTemperature();           // Read temperature in Celsius
+    
+    Serial.println("Reading LDR");
+    ldr = analogRead(LDR);
+    
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+
+  } else {
+
+    Serial.println("==================== Skipping ====================");
+  }
 }
